57710ddc79ebb1919640e6513b22b27502eb2a51
Raúl Marcelo Samaniego Villarreyes <rsamaniegov@pucp.edu.pe>
2024-06-08 12:24:07 -0500

    Create principal.ino
    se agregó la estructura del código principal con los pines que vamos a usar definidos

---
+++ Código_Proyecto2/Principal/principal/principal.ino
@@ -0,0 +1,136 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+//Pines para la tira led
+#define COLOR_LED 3   //Pin PWM para el color de los LED
+#define ON_OFF_LED A3 //Pin para habilitar alimentación de los LED
+#define NUMLEDS 8     //Número de LEDs en la tira 
+
+//Pines de habilitación de alimentación
+#define ON_OFF_BUZZER 4   //Pin habilitador de buzzer
+#define ON_OFF_PANTALLA 6 //Pin habilitador de pantalla OLED
+#define ON_OFF_SENSORES 7 //Pin que controla la alimentación de los sensores
+#define ON_OFF_VIB 10     //Pin habilitador de vibrador
+
+//Pines para habilitación de sensores (solo NO2 y CO)
+#define EN_CO 8
+#define EN_NO2 9  
+
+//Pin de interrupción
+#define INT0 2 //Pin de interrupción
+
+//Pines de lectura del ADC
+#define BAT_VOLT A6 //Pin de lectura de la tensión en la batería
+#define CO_OUT  A0  //Pin de lectura de valor de sensor CO
+#define NO2_OUT  A1 //Pin de lectura de valor de sensor NO2
+#define O2_OUT  A2  //Pin de lectura de valor de sensor O2
+
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMLEDS, COLOR_LED, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(COLOR_LED, OUTPUT);
+  pinMode(ON_OFF_BUZZER, OUTPUT);
+  pinMode(ON_OFF_PANTALLA, OUTPUT);
+  pinMode(ON_OFF_SENSORES, OUTPUT);
+  pinMode(ON_OFF_VIB, OUTPUT);
+  pinMode(INT0, INPUT);
+  pinMode(ON_OFF_LED, OUTPUT);
+  pinMode(EN_CO, OUTPUT);
+  pinMode(EN_NO2, OUTPUT);
+
+  pixels.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+ //controla la intensidad de cada color ingresado(0-100%)y devuelve el comando pixels con la intensidad adecuada
+uint32_t color_intensity(uint32_t color, uint8_t scale) {
+  uint8_t r = (color >> 16) & 0xFF;
+  uint8_t g = (color >> 8) & 0xFF;
+  uint8_t b = color & 0xFF;
+  r = (r * scale) / 100;
+  g = (g * scale) / 100;
+  b = (b * scale) / 100;
+  return pixels.Color(r, g, b);
+}
+
+//patrón de luces de inicio del dispositivo
+//enciende y apaga atenuando los colores del arreglo colors[]
+void startupPattern() {
+  uint32_t colors[] = {pixels.Color(0, 0, 255), pixels.Color(0, 255, 0), pixels.Color(255, 0, 0)};
+  int numColors = sizeof(colors) / sizeof(colors[0]);
+
+  for(int j = 0; j < numColors; j++) {
+    for(int brightness = 0; brightness <= 100; brightness++) {
+      for(int i = 0; i < NUMLEDS; i++) {
+        pixels.setPixelColor(i, color_intensity(colors[j], brightness));
+      }
+      pixels.show();
+      delay(8);
+    }
+
+    delay(300);
+
+    for(int brightness = 100; brightness >= 0; brightness--) {
+      for(int i = 0; i < NUMLEDS; i++) {
+        pixels.setPixelColor(i, color_intensity(colors[j], brightness));
+      }
+      pixels.show();
+      delay(8);
+    }
+  }
+
+  for(int brightness = 100; brightness >= 0; brightness--) {
+    for(int i = 0; i < NUMLEDS; i++) {
+      pixels.setPixelColor(i, pixels.Color(brightness, brightness, brightness));
+    }
+    pixels.show();
+    delay(8);
+  }
+}
+
+void vib_inicio() {
+  digitalWrite(ON_OFF_VIB, HIGH);
+  delay(1000);
+  digitalWrite(ON_OFF_VIB, LOW);
+  delay(1000);
+}
+
+void vib_lectura_alta() {
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(ON_OFF_VIB, HIGH);
+    delay(100);
+    digitalWrite(ON_OFF_VIB, LOW);
+    delay(100);
+  }
+}
+
+void vib_lectura_moderada() {
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ON_OFF_VIB, HIGH);
+    delay(250);
+    digitalWrite(ON_OFF_VIB, LOW);
+    delay(250);
+  }
+}
+
+void vib_lectura_normal() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ON_OFF_VIB, HIGH);
+    delay(500);
+    digitalWrite(ON_OFF_VIB, LOW);
+    delay(500);
+  }
+}
+
+
+
+
+
+
+
