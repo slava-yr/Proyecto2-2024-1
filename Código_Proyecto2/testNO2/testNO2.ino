2899ba3da1542b0791c0a6d249ba56c7c8db5ebf
Lurigon <sermaoz8a@gmail.com>
2024-06-14 14:49:44 -0500

    .

--- Código_Proyecto2/testNO2/testNO2.ino
+++ Código_Proyecto2/testNO2/testNO2.ino
@@ -2,18 +2,23 @@
 
 #define CALIBRATION_TIME 3 // Tiempo de calibración predeterminado de tres minutos
 
-// Direcciones I2C para los sensores
-#define MICS_I2C_ADDRESS_1 MICS_ADDRESS_0
-#define MICS_I2C_ADDRESS_2 MICS_ADDRESS_1
+// Definición de pines para los sensores
+#define SENSOR1_ADC_PIN   A1
+#define SENSOR1_POWER_PIN 10
+#define SENSOR2_ADC_PIN   A2
+#define SENSOR2_POWER_PIN 11
 
-DFRobot_MICS_I2C mics1(&Wire, MICS_I2C_ADDRESS_1);
-DFRobot_MICS_I2C mics2(&Wire, MICS_I2C_ADDRESS_2);
+DFRobot_MICS_ADC mics1(SENSOR1_ADC_PIN, SENSOR1_POWER_PIN);
+DFRobot_MICS_ADC mics2(SENSOR2_ADC_PIN, SENSOR2_POWER_PIN);
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
   // Inicialización del primer sensor
+  pinMode(SENSOR1_POWER_PIN, OUTPUT);
+  digitalWrite(SENSOR1_POWER_PIN, HIGH);
+
   while (!mics1.begin()) {
     Serial.println("No Devices for Sensor 1!");
     delay(1000);
@@ -29,6 +34,9 @@ void setup() {
   }
 
   // Inicialización del segundo sensor
+  pinMode(SENSOR2_POWER_PIN, OUTPUT);
+  digitalWrite(SENSOR2_POWER_PIN, HIGH);
+
   while (!mics2.begin()) {
     Serial.println("No Devices for Sensor 2!");
     delay(1000);
@@ -57,14 +65,14 @@ void setup() {
 
 void loop() {
   // Lectura de datos del primer sensor
-  float gasdata1 = mics1.getGasData(NO2);
-  Serial.print("Sensor 1 NO2 Value: ");
+  float gasdata1 = mics1.getGasData(C2H5OH);
+  Serial.print("Sensor 1 Ethanol Value: ");
   Serial.print(gasdata1, 1);
   Serial.println(" PPM");
 
   // Lectura de datos del segundo sensor
-  float gasdata2 = mics2.getGasData(NO2);
-  Serial.print("Sensor 2 NO2 Value: ");
+  float gasdata2 = mics2.getGasData(C2H5OH);
+  Serial.print("Sensor 2 Ethanol Value: ");
   Serial.print(gasdata2, 1);
   Serial.println(" PPM");
 
